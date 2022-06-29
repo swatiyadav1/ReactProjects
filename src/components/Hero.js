ab659ac85f96d4dc0e10355943845cf2adf1f2fb
swatiyadav1 <108142625+swatiyadav1@users.noreply.github.com>
2022-06-29 19:39:17 +0530

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../style.css"
+import phgrid from "../images/photo-grid.png"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src={phgrid} className="hero--photo" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
